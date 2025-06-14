33a365feaef002d1847f9c1a72daa22e23f8445a
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-06-14 20:58:23 +0530

    Update admin.py

--- ipo_app/admin.py
+++ ipo_app/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import IPO
 
-admin.site.register(IPO)
+@admin.register(IPO)
+class IPOAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'status', 'listing_date', 'ipo_price', 'listing_price')
+    list_filter = ('status', 'listing_date')
+    search_fields = ('company_name', 'issue_size')
