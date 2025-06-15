565bf556ccc4f1bbed9e6ab0a77b419aa4eab6bf
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-06-15 09:30:38 +0530

    Update settings.py

--- ipo_project/settings.py
+++ ipo_project/settings.py
@@ -13,8 +13,16 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition (same as before)
 INSTALLED_APPS = [
-    ...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'ipo_app',
 ]
+
 
 # Middleware (same as before)
 MIDDLEWARE = [
