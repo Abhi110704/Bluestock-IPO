4f7d8e98fab7d911da491209e27c48afc93c08d9
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-06-17 01:06:44 +0530

    Update urls.py

--- ipo_project/urls.py
+++ ipo_project/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from ipo_app.views import ReactAppView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ipo_app.urls')),  # ✅ Add this for home route
-    path('api/', include('ipo_app.urls')),  # Already for API
+    path('', ReactAppView.as_view(), name='home'),
+    path('api/', include('ipo_app.urls')),
 ]
 
 if settings.DEBUG:
