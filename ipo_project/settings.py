bb69cc6a8d559144c81fc90989a88d3bf31af98e
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-06-17 01:06:21 +0530

    Update settings.py

--- ipo_project/settings.py
+++ ipo_project/settings.py
@@ -46,7 +46,9 @@ ROOT_URLCONF = 'ipo_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            BASE_DIR / 'frontend' / 'build',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -88,7 +90,10 @@ USE_TZ = True
 
 # Static and Media files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'frontend' / 'build' / 'static',
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
