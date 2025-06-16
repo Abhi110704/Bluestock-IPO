cdfe635dba23672746080d965314a161cc5ae725
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-06-17 01:40:48 +0530

    Update serializers.py

--- ipo_app/serializers.py
+++ ipo_app/serializers.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 from .models import IPO
 
 class IPOSerializer(serializers.ModelSerializer):
-    issue_size = serializers.SerializerMethodField()
+    issue_size_display = serializers.SerializerMethodField()
 
     class Meta:
         model = IPO
         fields = '__all__'
 
-    def get_issue_size(self, obj):
+    def get_issue_size_display(self, obj):
         return f"₹{obj.issue_size} Crore"
