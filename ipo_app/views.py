fa68dcc5e7e6bbaff8e8e08656d923730e556538
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-06-17 01:03:43 +0530

    Update views.py

--- ipo_app/views.py
+++ ipo_app/views.py
@@ -2,21 +2,41 @@ from rest_framework import viewsets, filters
 from .models import IPO
 from .serializers import IPOSerializer
 from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from rest_framework.pagination import PageNumberPagination
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class IPOViewSet(viewsets.ModelViewSet):
     queryset = IPO.objects.all().order_by('-open_date')
     serializer_class = IPOSerializer
+    pagination_class = StandardResultsSetPagination
 
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['company_name', 'issue_type', 'status']
     ordering_fields = ['listing_date', 'ipo_price', 'current_market_price']
 
-from django.shortcuts import render
-from .models import IPO
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
 
-def ipo_list_view(request):
-    ipos = IPO.objects.all().order_by('-open_date')
-    return render(request, 'ipo_app/ipo_list.html', {'ipos': ipos})
+        serializer = self.get_serializer(queryset, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+class ReactAppView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 def ipo_chart_data(request):
     # TODO: Replace with real data from your models
